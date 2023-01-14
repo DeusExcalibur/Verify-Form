587f149e37f6695a1eae34bd9bce82bc1e8766e9
David Segura Vesga <dseguravesga@gmail.com>
2023-01-14 02:10:55 -0500

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true
+  },
+  excludes: ['libs'],
+  extends: 'standard',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+  }
+}
--- index.js
+++ index.js
@@ -1,30 +1,34 @@
-const firstName = document.getElementById("firstName");
-const lastName = document.getElementById("lastName");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const form = document.getElementById("form");
-const error = document.getElementById("error");
+const firstName = document.getElementById('firstName')
+const lastName = document.getElementById('lastName')
+//  const email = document.getElementById('email')
+const password = document.getElementById('password')
+const form = document.getElementById('form')
+const error = document.getElementById('error')
 
-form.addEventListener("submit", e => {
-  let messages = []
-  if (firstName.value === '' || firstName.value == null) {
-    messages.push(`Firstname is required`);
-  }
+const hasValue = (obj) => obj.value === '' || obj.value == null || obj.value === undefined
 
-  if (lastName.value === '' || lastName.value == null) {
-    messages.push(`Lastname is required`);
-  }
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  const messages = []
+  const requireMessage = (value) => messages.push(`${value} is required`)
 
-  
-  
+  /**
+   * Tu piensa que el futuro tiene que cambiar cosas tipo
+   * ahora el valor es undefined pues solo lo tendrias que cambiar una vez
+   * seria mas comodo para ti y tus compañeros.
+   */
+  if (hasValue(firstName)) requireMessage('First Name')
+  if (hasValue(lastName)) requireMessage('Last Name')
+
+  // Esto de aqui si sabes usar css esto daria igual ponerlo
   if (password.value.length <= 6) {
     messages.push('Password must be longer than 6 characters')
   }
 
   if (messages.length > 0) {
-    e.preventDefault()
-    error.innerText = messages.join(', ')
+    new Toast({
+      message: messages.join(', '),
+      type: 'danger'
+    });
   }
-
-
-});
+})
